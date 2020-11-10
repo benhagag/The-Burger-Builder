b393d8bbb5a07f4df93dc2524226f671f765415b
Ben Hagag <benbenhagag@google.com>
2020-11-10 12:49:17 +0200

    Outputing mulirplr build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
 
-buildControls = props => {
-    <div>
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+];
 
+const buildControls = props => (
+    <div className={classes.BuildControls}>
+        {
+            controls.map(ctrl => (
+                <BuildControl key={ctrl.Label} label={ctrl.label} />
+            ))
+        }
     </div>
-}
+)
 
 export default buildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Auxiliary from '../../HOC/Auxiliary';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 
 class BurgerBuilder extends Component {
@@ -25,7 +26,7 @@ class BurgerBuilder extends Component {
         return(
             <Auxiliary>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls />
             </Auxiliary>
         );
     }
