7a2d74dbffaef3a485121dfd2155afbbbb9c15d3
Ben Hagag <benbenhagag@google.com>
2020-11-16 01:25:19 +0200

    Creating a responsive sideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Auxiliary from '../../HOC/Auxiliary';
 import Classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => (
 
             // using Auxiliary HOC and get the props.childern from it
             <Auxiliary>
                <Toolbar />
+               <SideDrawer />
                 <main className={Classes.Content}>
                     {props.children}
                 </main>
