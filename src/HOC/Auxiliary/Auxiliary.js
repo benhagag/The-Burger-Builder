600744f49c8898e95c8f7d155f97767f57cb23c0
Ben Hagag <benbenhagag@gmail.com>
2024-08-05 23:48:43 +0300

    Updates

--- src/HOC/Auxiliary/Auxiliary.js
+++
@@ -1,8 +0,0 @@
-/**
- * Fucntional Component aux (Auxiliary)
- * Get props as in input and rertun props.children
- */
-
-const aux  = (props) => props.children;
-
-export default aux;
--- src/axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'https://the-burger-builder-66f55.firebaseio.com/'
-});
-
-export default instance;
